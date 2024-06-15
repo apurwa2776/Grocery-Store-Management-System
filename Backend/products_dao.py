df0cb1da3305347766aa5560bb8f0744ff762e9e
Apurwa Chaurasia <apurwa2776@gmail.com>
2024-06-15 18:57:32 +0530

    add

--- Backend/products_dao.py
+++ Backend/products_dao.py
@@ -1,13 +1,44 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='Apurwa@2776',
-                              host='127.0.0.1',
-                              database='grocerry_store')
-cursor = cnx.cursor()
-query = "SELECT * FROM grocerry_store.products"
-cursor.execute(query)
+def get_all_products():
+    # Establish the connection
+    cnx = mysql.connector.connect(
+        user='root', 
+        password='Apurwa@2776',
+        host='127.0.0.1',
+        database='grocerry_store'
+    )
+    cursor = cnx.cursor()
 
-for (product_id, name, uom_id,price_per_unit) in cursor:
-     print(product_id, name, uom_id,price_per_unit)
+    # Define your query
+    query = (
+        "SELECT products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name "
+        "FROM products "
+        "INNER JOIN uom ON products.uom_id = uom.uom_id"
+    )
 
-cnx.close()
+    # Execute the query
+    cursor.execute(query)
+
+    response = []
+
+    # Fetch and process the results
+    for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
+        response.append({
+            'product_id': product_id,
+            'name': name,
+            'uom_id': uom_id,
+            'price_per_unit': price_per_unit,
+            'uom_name': uom_name
+        })
+
+    # Close the cursor and connection
+    cursor.close()
+    cnx.close()
+
+    return response
+
+if __name__ == '__main__':
+    products = get_all_products()
+    for product in products:
+        print(product)
